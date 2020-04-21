5d37e48aa9da686a1893a849f4771cf82b9d7acc
Àlex Resta <arestama@everis.com>
2020-04-21 23:19:47 +0200

    +Fix només verifica canvis en articles 1 cop al dia +Menu +Exportar CSV

--- debugDB.py
+++ debugDB.py
@@ -1,34 +1,126 @@
 from tinydb import TinyDB, Query
 from datetime import date
-
+import sys
+import os
+from mathom.spiders.mathom import revisarofertes
+from colorama import Fore, Back, Style, init, deinit
 
 db = TinyDB('db.json')
+NEW_LINE = "\n"
 
-#db.purge()
+def veureresultats():
 
-Cerca = Query()
-            
-results = db.search(Cerca.status_stock == "NOU")
+    Cerca = Query()
+                
+    results = db.search(Cerca.status_stock == "NOU")
 
-print("JOCS NOUS: \n")
-for producte in results:
-    print(producte['nom'])
+    print("JOCS NOUS: \n")
+    for producte in results:
+        print(producte['nom'])
+        
+    print("----------------\n")
+
+    results = db.search(Cerca.status_preu == "REBAIXAT")
+
+    print("JOCS REBAIXATS: \n")
+    for producte in results:
+        print(producte['nom'])
+    print("----------------\n")
+         
+    results = db.search(Cerca.date_lastseen < date.today().isoformat())
+
+    print("JOCS DESAPAREGUTS: \n")
+    for producte in results:
+        print(producte['nom'])
+    print("----------------\n")     
+     
+    results = db.search(Cerca.stock != "Agotado")
+
+     
+    print("TOTAL STOCK / ESGOTATS: " )     
+    print(str(len(results)) + "/" +str(len(db.all())))
+ 
+    input("Prem Enter per continuar")
+    mainmenu()
+ 
+
+def extreureCSV():
+
+    f= open(F"jocsoferta.csv","w")
+    f.write("\"Nom\";\"Preu\";\"URL\"" + NEW_LINE)
+
+    Cerca = Query()
+
+    results = db.search(Cerca.stock != "Agotado")
+
+    for producte in results:
+        f.write(F"\"{producte['nom']}\";\"{producte['preu']}\";\"{producte['url']}\"" + NEW_LINE)
+        
+        
+    print()
+    print("FET!")
+    print()
+
+
+    input("Prem Enter per continuar")
+    mainmenu()
     
-print("----------------\n")
+def mathomofertes():
+    revisarofertes()
+    
+    input("Prem Enter per continuar")
+    mainmenu()
+    
+    
+def deleteDB():
+    print ("deleting!")
+    #db.purge()
+    input("Prem Enter per continuar")
+    mainmenu()
+    
+    
+def mainmenu():
+    os.system('cls')
+    print(Style.RESET_ALL)
+    
+    print(Fore.WHITE + Back.BLUE)
+    print("************MATHOM MENU**************")
+    print(Style.RESET_ALL) 
 
-results = db.search(Cerca.status_preu == "REBAIXAT")
+    choice = input("""
+      1: Veure resultats
+      2: Treure CSV de tot
+      3: Revisar ofertes
+      K: Delete DB
+      Q: Sortir
+    
+      ?: """)
 
-print("JOCS REBAIXATS: \n")
-for producte in results:
-    print(producte['nom'])
-print("----------------\n")
-     
-results = db.search(Cerca.date_lastseen < date.today().isoformat())
+    if choice == "1":
+        veureresultats()
+    elif choice == "2":
+        extreureCSV()
+    elif choice == "3":
+        mathomofertes()
+    elif choice == "K" or choice =="k":
+        deleteDB()
+    elif choice=="Q" or choice=="q":
+        deinit()
+        sys.exit
+    else:
+        print()
+        print()
 
-print("JOCS DESAPAREGUTS: \n")
-for producte in results:
-    print(producte['nom'])
-print("----------------\n")     
-     
-print("TOTAL: " )     
-print(len(db.all()))
+        print(F"      {Fore.WHITE + Back.RED}Escull una opció del menú{Style.RESET_ALL}")
+
+        print()
+
+        input(F"      Prem {Fore.BLACK + Back.WHITE}RETORN{Style.RESET_ALL} per continuar")
+        mainmenu()
+    
+  
+init()  
+mainmenu()
+    
+   
+
--- mathom/spiders/mathom.py
+++ mathom/spiders/mathom.py
@@ -3,8 +3,10 @@ from mathom.items import Producte
 from scrapy.loader import ItemLoader
 from tinydb import TinyDB, Query
 from datetime import date
+from scrapy.crawler import CrawlerProcess
 
-class QuotesSpider(scrapy.Spider):
+
+class MathomOfertes(scrapy.Spider):
     name = "mathom"
     
  
@@ -48,41 +50,55 @@ class QuotesSpider(scrapy.Spider):
                 
                 db.insert(producte)
             else:
-                producteDB['date_lastseen'] = date.today().isoformat()
+                if producteDB['date_lastseen'] < date.today().isoformat():
+                                
+                    producteDB['date_lastseen'] = date.today().isoformat()
 
-                if producteDB['stock'] == 'Agotado' and producte['stock'] == 'En stock':
-                    producteDB['status_stock'] = 'RESTOCK'
-                    producteDB['stock'] = producte['stock']
-                    producteDB['date_updated'] = date.today().isoformat()
-                elif producteDB['stock'] == 'En stock' and producte['stock'] == 'Agotado':
-                    producteDB['status_stock'] = 'ESGOTAT'
-                    producteDB['stock'] = producte['stock']
-                    producteDB['date_updated'] = date.today().isoformat()
-                elif producteDB['status_stock'] == 'NOU':
-                    producteDB['status_stock'] = 'STOCK'
-                    producteDB['date_updated'] = date.today().isoformat()
-                    
-                if producteDB['preu'] >  producte['preu']:
-                    producteDB['status_preu'] = 'REBAIXAT'
-                    producteDB['preu'] = producte['preu']
-                    producteDB['date_updated'] = date.today().isoformat()
-                elif producteDB['preu'] <  producte['preu']:
-                    producteDB['status_preu'] = 'FIPROMO'
-                    producteDB['preu'] = producte['preu']
-                    producteDB['date_updated'] = date.today().isoformat()
-                elif producteDB['preu'] ==  producte['preu'] and producteDB['status_preu'] != "IGUAL":
-                    producteDB['status_preu'] = 'IGUAL'
-                    producteDB['date_updated'] = date.today().isoformat() 
-                    
-                db.upsert(dict(producteDB),Cerca.url == producte['url'])
+                    if producteDB['stock'] == 'Agotado' and producte['stock'] == 'En stock':
+                        producteDB['status_stock'] = 'RESTOCK'
+                        producteDB['stock'] = producte['stock']
+                        producteDB['date_updated'] = date.today().isoformat()
+                    elif producteDB['stock'] == 'En stock' and producte['stock'] == 'Agotado':
+                        producteDB['status_stock'] = 'ESGOTAT'
+                        producteDB['stock'] = producte['stock']
+                        producteDB['date_updated'] = date.today().isoformat()
+                    elif producteDB['status_stock'] == 'NOU':
+                        producteDB['status_stock'] = 'STOCK'
+                        producteDB['date_updated'] = date.today().isoformat()
+                        
+                    if producteDB['preu'] >  producte['preu']:
+                        producteDB['status_preu'] = 'REBAIXAT'
+                        producteDB['preu'] = producte['preu']
+                        producteDB['date_updated'] = date.today().isoformat()
+                    elif producteDB['preu'] <  producte['preu']:
+                        producteDB['status_preu'] = 'FIPROMO'
+                        producteDB['preu'] = producte['preu']
+                        producteDB['date_updated'] = date.today().isoformat()
+                    elif producteDB['preu'] ==  producte['preu'] and producteDB['status_preu'] != "IGUAL":
+                        producteDB['status_preu'] = 'IGUAL'
+                        producteDB['date_updated'] = date.today().isoformat() 
+                        
+                    db.upsert(dict(producteDB),Cerca.url == producte['url'])
                 
-            #db.upsert(dict(producte),cerca.url == producte['url'])
-            #yield producte
+
  
         
         #PAGINES SEGÜENTS
-#       for next_page in response.css('a[rel=next]'):
-#           yield response.follow(next_page, self.parse)
+        for next_page in response.css('a[rel=next]'):
+            pass
+            yield response.follow(next_page, self.parse)
             
             
           #response.xpath("//meta[@name='keywords']/@content")[0].extract()
+        
+def revisarofertes():
+    print ("fent ofertes...")
+    
+    process = CrawlerProcess(settings={
+    'FEED_FORMAT': 'json',
+    'FEED_URI': 'items.json'
+    })
+
+   
+    process.crawl(MathomOfertes)
+    process.start() # the script will block here until the crawling is finished
