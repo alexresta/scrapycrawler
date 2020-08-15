323759be40256d212da1e44b1a85b9b9b25cc0c7
Alex Resta <arestama@everis.com>
2020-08-15 15:00:37 +0200

    fix: remove logs

--- scrapeengines/spiders/ecicataleg.py
+++ scrapeengines/spiders/ecicataleg.py
@@ -39,7 +39,7 @@ class EciCataleg(scrapy.Spider):
 
     def parse(self, response):
         page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+       #filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
--- scrapeengines/spiders/juguetestoday.py
+++ scrapeengines/spiders/juguetestoday.py
@@ -36,7 +36,7 @@ class JuguetestodayCataleg(scrapy.Spider):
 
     def parse(self, response):
         page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+        #filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
