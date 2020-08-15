b824dfe19f45c6df50ede90bebeb4593e791e0e5
Alex Resta <arestama@everis.com>
2020-08-15 15:14:44 +0200

    fix indentation

--- scrapeengines/items.py
+++ scrapeengines/items.py
@@ -66,10 +66,10 @@ class Producte(scrapy.Item):
                 self['preu'] = 0
 
         if "preu_original" in self:
-			try:
-			    self['preu_original'] = float(self['preu_original'])
-			except:
-			    self['preu_original'] = float(0)
+            try:
+                self['preu_original'] = float(self['preu_original'])
+            except:
+                self['preu_original'] = float(0)
   
         else:
             self['preu'] = float(self['preu'])
