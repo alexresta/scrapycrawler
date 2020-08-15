1dfaf605b89e09823099657961192e79fdc70c02
Alex Resta <arestama@everis.com>
2020-08-15 15:21:07 +0200

    fix indent

--- scrapeengines/spiders/outletpccataleg.py
+++ scrapeengines/spiders/outletpccataleg.py
@@ -33,7 +33,7 @@ class OutletpcCataleg(scrapy.Spider):
 
     def parse(self, response):
         #page = response.url.split("/")[-2]
-		#filename = 'quotes-%s.html' % page
+        #filename = 'quotes-%s.html' % page
         #with open(filename, 'wb') as f:
         #    f.write(response.body)
         #self.log('Saved file %s' % filename)
